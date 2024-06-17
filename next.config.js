54f39144f974ae57c55f426c480a50b33bbbb969
mdv.gold.brawler <97101594+DiMo1999@users.noreply.github.com>
2024-06-17 21:52:26 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = {
   env: {
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: '75',
+    nextImageExportOptimizer_quality: 75,
     nextImageExportOptimizer_storePicturesInWEBP: 'true',
     nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
 
